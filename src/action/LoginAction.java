41dc6213659173fd70776e95fde0d83867be4681
Prasher <I331355@global.corp.sap>
2016-08-03 19:31:31 +0530

    Added login and logout functionality

---
+++ src/action/LoginAction.java
@@ -0,0 +1,46 @@
+package action;
+
+import model.User;
+import javax.servlet.http.HttpSession;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionRedirect;
+
+
+/**
+ * Class LoginAction, this is the action class
+ * for authenticating a valid user
+ *
+ * @author Prateek Prasher
+ * @author prateekprshr@gmail.com
+ */
+public class LoginAction extends Action{
+    public ActionRedirect execute(ActionMapping mapping, ActionForm form,
+                                  HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("logged") != null) {
+            return new ActionRedirect(mapping.findForward("taskView"));
+        } else {
+            String username = (String) request.getParameter("username");
+            String password = (String) request.getParameter("password");
+
+            User user = User.find(username);
+
+            if(user == null || !password.equals(user.getPassword())) {
+                session.setAttribute("error", "The credentials are invalid!!!");
+
+                return new ActionRedirect(mapping.findForward("taskView"));
+            } else {
+                session.setAttribute("logged", "");
+                session.setAttribute("username", user.getUsername());
+                session.removeAttribute("error");
+
+                return new ActionRedirect(mapping.findForward("taskView"));
+            }
+        }
+    }
+}
---
+++ src/action/LogoutAction.java
@@ -0,0 +1,31 @@
+package action;
+
+import javax.servlet.http.HttpSession;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionRedirect;
+
+
+/**
+ * Class LogoutAction, this is the action class
+ * for loggin out a valid user
+ *
+ * @author Prateek Prasher
+ * @author prateekprshr@gmail.com
+ */
+public class LogoutAction extends Action {
+    public ActionRedirect execute(ActionMapping mapping, ActionForm form,
+                                  HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("logged") != null) {
+            session.removeAttribute("logged");
+            session.removeAttribute("username");
+        }
+
+        return new ActionRedirect(mapping.findForward("taskView"));
+    }
+}
